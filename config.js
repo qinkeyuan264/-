feffcbf6bfb835d1845adbd576b2c41bba4b60d9
qinkeyuan264 <qinkeyuan264@gmail.com>
2026-04-17 22:15:21 +0400

    add site

---
+++ config.js
@@ -0,0 +1,9 @@
+// 这是“入口密码”的 SHA-256（小写 hex）。
+// 默认密码是 changeme123，对应哈希如下；建议你部署后立刻更换。
+// 更换方法：打开 /tools/hash.html 生成哈希，然后替换 PASSWORD_SHA256_HEX。
+window.__SITE_CONFIG__ = {
+  PASSWORD_SHA256_HEX:
+    "312332d0575efcc1c5aa536d2b7a12be8d71d5fbc6dacd46e06ecaeb0f638e5b",
+  AUTH_CACHE_DAYS: 7,
+  FILES_JSON_PATH: "./data/files.json",
+};
